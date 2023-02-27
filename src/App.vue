90879a6ac0e733b7edfc740f10ea603116af645e
hey-ri <kwonhr01@naver.com>
2023-02-27 14:11:05 +0900

    binding the function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <h1 class="name">Hello {{ name }} site</h1>
+  <h1 class="name">Hello {{ greeting() }} site</h1>
+  <h1 class="name">Hello {{ greeting2("hori") }} site</h1>
+  <h1 class="name">Hello {{ greeting2(name) }} site</h1>
+  <h1 class="name">Hello {{ greet }} site</h1>
 </template>
 
 <script>
@@ -7,8 +11,21 @@ export default {
   setup() {
     const name = "hey-ri";
 
+    const greeting = () => {
+      return "hari, ";
+    };
+
+    const greeting2 = (name) => {
+      return "hello, " + name;
+    };
+
+    const greet = greeting(name);
+
     return {
       name,
+      greeting,
+      greeting2,
+      greet,
     };
   },
 };
@@ -16,6 +33,6 @@ export default {
 
 <style>
 .name {
-  color: antiquewhite;
+  color: #5a4c3a;
 }
 </style>
