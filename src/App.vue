c82abd25b8a4118c72dc4cf418d6ac3a47b1b455
hey-ri <kwonhr01@naver.com>
2023-02-27 14:18:05 +0900

    Write the message from console.log when click the button

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
   <h1 class="name">Hello {{ name }} site</h1>
-  <h1 class="name">Hello {{ greeting() }} site</h1>
+  <!-- <h1 class="name">Hello {{ greeting() }} site</h1>
   <h1 class="name">Hello {{ greeting2("hori") }} site</h1>
   <h1 class="name">Hello {{ greeting2(name) }} site</h1>
-  <h1 class="name">Hello {{ greet }} site</h1>
+  <h1 class="name">Hello {{ greet }} site</h1> -->
+  <button v-on:click="consleLog" class="btn btn-primary">Click</button>
+  <button @click="updateName" class="btn btn-primary">change name</button>
 </template>
 
 <script>
 export default {
   setup() {
-    const name = "hey-ri";
+    let name = "hey-ri";
 
-    const greeting = () => {
-      return "hari, ";
+    // const greeting = () => {
+    //   return "hari, ";
+    // };
+
+    // const greeting2 = (name) => {
+    //   return "hello, " + name;
+    // };
+
+    // const greet = greeting(name);
+    const consleLog = () => {
+      console.log("hello world");
     };
 
-    const greeting2 = (name) => {
-      return "hello, " + name;
+    const updateName = () => {
+      name = "jane";
+      console.log(name);
     };
-
-    const greet = greeting(name);
 
     return {
       name,
-      greeting,
-      greeting2,
-      greet,
+      // greeting,
+      // greeting2,
+      // greet,
+      consleLog,
+      updateName,
     };
   },
 };
