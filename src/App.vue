bc274900ca1c36c7f0a6eb5424eddde851179005
hey-ri <kwonhr01@naver.com>
2023-02-28 17:17:12 +0900

    use a async await
    when axios is long it is cause callback hell

--- src/App.vue
+++ src/App.vue
@@ -27,25 +27,30 @@ export default {
     const todoList = ref([]);
     const error = ref('');
 
-    const addTodo = (todo) => {
+    const addTodo = async (todo) => {
       //인자 todo는 자식 컴포넌트에서 받아온 것을 의미함
       console.log(todo);
       error.value = '';
       //데이터 베이스 투두를 저장하기
-      axios
-        .post('http://localhost:3000/todos', {
+      try {
+        const res = await axios.post('http://localhost:3000/todos', {
           subject: todo.subject,
           completed: todo.completed,
-        })
-        .then((res) => {
-          //.then을 해주는 이유는 axios는 promise로 값을 반환하는데(비동기적) 그 행동이 끝나고 완료되었을 때~(.then일때~)그때 무슨 일을 해라 라는 의미로 사용됨
-          console.log(res);
-          todoList.value.push(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-          error.value = 'Somthing went wrong';
         });
+        todoList.value.push(res);
+      } catch (err) {
+        console.log(err);
+        error.value = 'Somthing went wrong';
+      }
+      // .then((res) => {
+      //   //.then을 해주는 이유는 axios는 promise로 값을 반환하는데(비동기적) 그 행동이 끝나고 완료되었을 때~(.then일때~)그때 무슨 일을 해라 라는 의미로 사용됨
+      //   console.log(res);
+      //   todoList.value.push(res.data);
+      // })
+      // .catch((err) => {
+      //   console.log(err);
+      //   error.value = 'Somthing went wrong';
+      // });
     };
 
     const deleteTodo = (index) => {
