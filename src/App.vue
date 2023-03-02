e464a240ab70d0e9d8aae7712b7f283ffff0a08f
hey-ri <kwonhr01@naver.com>
2023-03-02 11:48:43 +0900

    Synchronous pagination and button visibility conditions

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,13 @@
     <nav aria-label="Page navigation example">
       <ul class="pagination">
         <li class="page-item">
-          <a class="page-link" href="#"> Previous </a>
+          <a class="page-link" href="#" v-if="currentPage !== 1"> Previous </a>
+        </li>
+        <li v-for="page in totalPage" :key="page" class="page-item" :class="currentPage === page ? 'active' : ''">
+          <a class="page-link" href="#"> {{ page }}</a>
         </li>
         <li class="page-item">
-          <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">Next</a>
+          <a class="page-link" href="#" v-if="numberOfTodos !== currentPage">Next</a>
         </li>
       </ul>
     </nav>
@@ -46,18 +40,23 @@ export default {
   setup() {
     const todoList = ref([]);
     const error = ref('');
-    const totalPage = ref(0);
+    //마지막 페이지
+    const numberOfTodos = ref(0);
     const limit = 5;
-    const page = ref(1);
+    const currentPage = ref(1);
+
+    const totalPage = computed(() => {
+      return Math.ceil(numberOfTodos.value / limit);
+    });
 
     const getTodos = async () => {
       try {
-        const res = await axios.get(`http://localhost:3000/todos?_page=${page.value}&_limit=${limit}`);
+        const res = await axios.get(`http://localhost:3000/todos?_page=${currentPage.value}&_limit=${limit}`);
         // console.log(res.headers);
         //결과의 headers에서 x-total-count를 받아온다.
         // console.log(res.headers.x-total-count);
         //-로 이어져 있는 변수는 []로 감싸고 ''로 묶어줘야 한다.
-        totalPage.value = res.headers['x-total-count'];
+        numberOfTodos.value = res.headers['x-total-count'];
         todoList.value = res.data;
       } catch (err) {
         console.log(err);
@@ -146,6 +145,8 @@ export default {
       searchText,
       filterTodos,
       error,
+      totalPage,
+      currentPage,
     };
   },
 };
