c0dbbe0e63c1e3d4b29b8e7e2de17354d2d34814
hey-ri <kwonhr01@naver.com>
2023-03-02 16:28:02 +0900

    using a router-link for page routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="container">
-    <router-view />
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">Hey Ri</router-link>
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/todos">Todos <span class="sr-only">(current)</span></router-link>
+      </li>
+    </ul>
+  </nav>
+  <router-view />
 </template>
 
 <script>
