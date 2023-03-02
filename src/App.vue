60cdf92a69d0fe980cfe2e2e81738ed86adc4743
hey-ri <kwonhr01@naver.com>
2023-03-02 17:22:24 +0900

    routing this id when click the this card connect route is name (don't use path)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand" to="/">Hey Ri</router-link>
+    <router-link class="navbar-brand" :to="{ name: 'home' }">Hey Ri</router-link>
 
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <router-link class="nav-link" to="/todos">Todos <span class="sr-only">(current)</span></router-link>
+        <router-link class="nav-link" :to="{ name: 'todos' }">Todos</router-link>
       </li>
     </ul>
   </nav>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
-    <div class="card-body p-2 d-flex align-items-center">
+    <div class="card-body p-2 d-flex align-items-center" @click="moveToPage(todo.id)">
       <div class="form-check flex-grow-1">
         <label class="form-check-label" :class="{ todo: todo.completed }">
           <input class="form-check-input" type="checkbox" :checked="todo.completed" @change="toggleTodo(index)" />
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
 export default {
   props: {
     todos: {
@@ -22,6 +23,7 @@ export default {
   },
   emits: ['toggle-todo', 'todo-idx'],
   setup(props, { emit }) {
+    const router = useRouter();
     const toggleTodo = (index) => {
       emit('toggle-todo', index);
     };
@@ -30,9 +32,24 @@ export default {
       emit('todo-idx', index);
     };
 
+    const moveToPage = (todoId) => {
+      console.log(todoId);
+
+      // router.push('/todos/' + todoId);
+      //:to로 작성하기 위해서(:to를 사용하면 routes에서 name을 사용하는데, 그렇게 사용하면 나중에 path가 변경되었을 때 유지보수 할 때 편리함) :id를 받아오기 위해서는 밑에처럼 라우터를 푸시해주어야한다.
+      router.push({
+        name: 'todo',
+        params: {
+          id: todoId,
+          // id는 /router에 :id로 작성해주었기 때문에 id로 해야한다.
+        },
+      });
+    };
+
     return {
       toggleTodo,
       deleteTodo,
+      moveToPage,
     };
   },
 };
