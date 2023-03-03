e97d3e4b11cf73be5c05ae3f7fade3ba5cfd15f4
hey-ri <kwonhr01@naver.com>
2023-03-03 11:39:00 +0900

    create save function when the todo modify

--- src/pages/todos/_TodoId.vue
+++ src/pages/todos/_TodoId.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>To-Do Page</h1>
   <div v-if="loading">Loading...</div>
-  <form v-else>
+  <form v-else @submit.prevent="onSave">
     <div class="row">
       <div class="col-6">
         <div class="form-group">
@@ -44,9 +44,11 @@ export default {
     const loading = ref(true);
     const router = useRouter();
 
+    const todoId = route.params.id;
+
     const getTodo = async () => {
       //async await로 요청을 하고 받아오는 것을 비동기로 처리하고 결과를 res에 담아서 콘솔에서 확인한다. 그러고 나서 콘솔에서 확인 한 값을 가지고 또 다시 데이터에 접근 가능하게 만들 수 있다.
-      const res = await axios.get('http://localhost:3000/todos/' + route.params.id);
+      const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
 
       // console.log(res);
       todo.value = res.data;
@@ -65,11 +67,21 @@ export default {
       });
     };
 
+    const onSave = async () => {
+      const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+        subject: todo.value.subject,
+        completed: todo.value.completed,
+      });
+
+      console.log(res);
+    };
+
     return {
       todo,
       loading,
       toggleTodoStatus,
       moveToTodolistPage,
+      onSave,
     };
   },
 };
