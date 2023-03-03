b8fd59869f56c3a9ddb65ce87eed4c2efb0a3cb7
hey-ri <kwonhr01@naver.com>
2023-03-03 13:32:58 +0900

    create the toast component of status (success the update or error message)

--- src/pages/todos/_TodoId.vue
+++ src/pages/todos/_TodoId.vue
@@ -29,13 +29,19 @@
     <button type="submit" class="btn btn-outline-danger mr-2" @click="moveToTodolistPage">cancel</button>
     <button type="submit" class="btn btn-primary" :disabled="!todoUpdated">save</button>
   </form>
+  <Toast v-if="showToast" :message="toastMessage" :type="toastAlertType" />
 </template>
 <script>
 import axios from 'axios';
 import { ref, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import _ from 'lodash';
+import Toast from '@/components/Toast.vue';
+
 export default {
+  components: {
+    Toast,
+  },
   setup() {
     const route = useRoute();
     //클릭해서 들어온 todo page의 인덱스 번호를 나타내줌
@@ -43,18 +49,26 @@ export default {
     const todo = ref(null);
     const originalTodo = ref(null);
     const loading = ref(true);
+    const showToast = ref(false);
+    const toastMessage = ref('');
+    const toastAlertType = ref('');
     const router = useRouter();
 
     const todoId = route.params.id;
 
     const getTodo = async () => {
       //async await로 요청을 하고 받아오는 것을 비동기로 처리하고 결과를 res에 담아서 콘솔에서 확인한다. 그러고 나서 콘솔에서 확인 한 값을 가지고 또 다시 데이터에 접근 가능하게 만들 수 있다.
-      const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
+      try {
+        const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
 
-      // console.log(res);
-      todo.value = { ...res.data };
-      originalTodo.value = { ...res.data };
-      loading.value = false;
+        // console.log(res);
+        todo.value = { ...res.data };
+        originalTodo.value = { ...res.data };
+        loading.value = false;
+      } catch (err) {
+        console.log(err);
+        triggerToast('somthing went to wrong from server', 'danger');
+      }
     };
 
     const todoUpdated = computed(() => {
@@ -74,13 +88,31 @@ export default {
     getTodo();
 
     const onSave = async () => {
-      const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
-        subject: todo.value.subject,
-        completed: todo.value.completed,
-      });
+      try {
+        const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+          subject: todo.value.subject,
+          completed: todo.value.completed,
+        });
 
-      console.log(res);
-      originalTodo.value = { ...res.data };
+        console.log(res);
+        originalTodo.value = { ...res.data };
+        triggerToast('Successfully saved!');
+      } catch (err) {
+        console.log(err);
+        triggerToast('Something went to wrong from server', 'danger');
+      }
+    };
+
+    const triggerToast = (message, type = 'success') => {
+      toastMessage.value = message;
+      showToast.value = true;
+      toastAlertType.value = type;
+
+      setTimeout(() => {
+        toastAlertType.value = '';
+        toastMessage.value = 's';
+        showToast.value = false;
+      }, 2100);
     };
 
     return {
@@ -90,6 +122,9 @@ export default {
       moveToTodolistPage,
       onSave,
       todoUpdated,
+      showToast,
+      toastMessage,
+      toastAlertType,
     };
   },
 };
