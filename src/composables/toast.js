59f417be010da925839604361233f2a1c8578b36
hey-ri <kwonhr01@naver.com>
2023-03-06 16:14:12 +0900

    mutations, actions, getters use in vuex for triggerToast function

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -1,4 +1,4 @@
-import { computed, onUnmounted } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 
 export const useToast = () => {
@@ -6,30 +6,34 @@ export const useToast = () => {
 
   //computed 사용 이유, 값이 변경 된것을 감지하려고.
   const showToast = computed(() => store.state.showToast);
-  const toastMessage = computed(() => store.state.toastMessage);
+  const toastMessage = computed(() => store.getters.toastMessageWithSmile);
   const toastAlertType = computed(() => store.state.toastAlertType);
-  const timeOut = computed(() => store.state.timeOut);
+  // const timeOut = computed(() => store.state.timeOut);
+
   const triggerToast = (message, type = 'success') => {
-    toastMessage.value = message;
+    /*  toastMessage.value = message;
     showToast.value = true;
     toastAlertType.value = type;
-
+    
     timeOut.value = setTimeout(() => {
       console.log('셋 타임 아웃 실행중');
       toastAlertType.value = '';
       toastMessage.value = '';
       showToast.value = false;
-    }, 3000);
+    }, 3000); */
+
+    //vuex의 액션을 취하고 싶을 때는 dispatch
+    store.dispatch('triggerToast', message, type);
   };
 
-  onUnmounted(() => {
-    clearTimeout(timeOut.value);
-  });
+  //app.vue에서만 toast를 사용할 것이기 때문에 app.vue를 나간다는 것은 이 앱 자체를 나간다는 것이기 때문에 밑에줄이 필요 없어진다.
+  // onUnmounted(() => {
+  //   clearTimeout(timeOut.value);
+  // });
   return {
     showToast,
     toastMessage,
     toastAlertType,
     triggerToast,
-    onUnmounted,
   };
 };
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,45 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    showToast: true,
-    toastMessage: 'ddd',
+    showToast: false,
+    toastMessage: '',
     toastAlertType: '',
-    timeOut: null,
+  },
+  mutations: {
+    UPDATE_TOAST_MESSAGE(state, payload) {
+      state.toastMessage = payload;
+    },
+    UPDATE_TOAST_ALERT_TYPE(state, payload) {
+      state.toastAlertType = payload;
+    },
+    UPDATE_TOAST_STATUS(state, payload) {
+      state.showToast = payload;
+    },
+  },
+  actions: {
+    triggerToast({ commit }, message, type = 'success') {
+      // toastMessage.value = message;
+      commit('UPDATE_TOAST_MESSAGE', message);
+      // toastAlertType.value = type;
+      commit('UPDATE_TOAST_ALERT_TYPE', type);
+      // showToast.value = true;
+      commit('UPDATE_TOAST_STATUS', true);
+
+      setTimeout(() => {
+        console.log('셋 타임 아웃 실행중');
+        commit('UPDATE_TOAST_ALERT_TYPE', '');
+        // toastAlertType.value = '';
+        commit('UPDATE_TOAST_MESSAGE', '');
+        // toastMessage.value = '';
+        commit('UPDATE_TOAST_STATUS', false);
+        // showToast.value = false;
+      }, 3000);
+    },
+  },
+  getters: {
+    toastMessageWithSmile(state) {
+      //vuex 안에서 사용하는 computed와 같다.
+      return state.toastMessage + ' :)';
+    },
   },
 });
