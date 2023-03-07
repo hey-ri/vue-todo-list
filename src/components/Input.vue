2b8062d20ea950907e8cfd629b9298401b3bb09d
hey-ri <kwonhr01@naver.com>
2023-03-07 18:22:09 +0900

    add a description of v-model props

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,7 +2,7 @@
   <div class="form-group">
     <label>{{ label }}</label>
     <input :value="subject" @input="onInput" type="text" class="form-control" />
-    <!-- input에 원래 있던 v-model은 (v-model:"todo.subject") todo가 부모 컴포넌트에 존재함으로 사용할 수 없다. -->
+    <!-- input에 원래 있던 v-model은 (v-model:"todo.subject") todo가 부모 컴포넌트에 존재함으로 사용할 수 없다. 그래서 v-model의 두 역할 :value와 @input을 따로 사용함으로 해결중이다.-->
     <div v-if="error" class="text-error">{{ error }}</div>
   </div>
 </template>
@@ -28,7 +28,8 @@ export default {
     const { emit } = getCurrentInstance();
 
     const onInput = (e) => {
-      // console.log(e.target.value)
+      console.log(e.target.value);
+      //TodoForm에 v-model:subject에서 subject와 이름이 같아야 하고, 그래야 부모에서 onUpdated될 때 값을 업데이트 할 수 있음(onUpdated는 dom tree에 상태가 변경될때 변경됨)
       emit('update:subject', e.target.value);
       /* :하고 props의 이름을 적어줘야 v-model:subject를 한 의미가 있음 */
     };
